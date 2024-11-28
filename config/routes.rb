dca98bfc6766c14842f7e93158f2a4a408246568
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2024-11-28 16:51:13 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get "items/index"
-  get "items/show"
-  get "items/new"
-  get "items/edit"
   root "static_pages#top"
+
+  resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
