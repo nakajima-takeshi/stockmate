9c552fdcef8e68f42279f62c75eb1c3e661e0b9b
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2024-11-28 19:53:14 +0900

    LINEログイン機能をローカル環境下で実装

---
+++ db/migrate/20241128101944_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_28_092627) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_28_101944) do
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
     t.string "name"
@@ -29,6 +29,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_28_092627) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
