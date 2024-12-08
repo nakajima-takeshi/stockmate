04ffcedd3114031208cab433da6cec5f319412a0
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2024-12-08 19:25:28 +0900

    コードの崩れを修正

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -2,21 +2,21 @@ class NotificationController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def callback
-    body = request.body.read
-    events = client.parse_events_from(body)
-    events.each do |event|
-    case event
-    when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-                message = {
-                    type: "text",
-                    text: event.message["text"]
-                }
-                client.reply_message(event["replyToken"], message)
+        body = request.body.read
+        events = client.parse_events_from(body)
+        events.each do |event|
+            case event
+            when Line::Bot::Event::Message
+                case event.type
+                when Line::Bot::Event::MessageType::Text
+                    message = {
+                        type: "text",
+                        text: event.message["text"]
+                    }
+                    client.reply_message(event["replyToken"], message)
+                end
+            end
         end
-    end
-    end
     end
 
     private
