d2576e916601183923f401b0d48500127b8ce2b2
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2024-12-12 13:28:51 +0900

    手動で通知日の更新、エラーメッセージの表示を設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,14 +1,12 @@
 class NotificationsController < ApplicationController
     before_action :authenticate_user!
 
-    before_action :set_notifications, only: [:edit, :update]
+    before_action :set_notification, only: [:edit, :update]
 
     def edit
-        set_notifications
     end
 
     def update
-        set_notifications
         if @notification.update(notification_params)
             @notification.notification_update_next_notification_day(@notification.next_notification_day)
             redirect_to items_path, notice: '通知日を更新しました'
@@ -19,18 +17,14 @@ class NotificationsController < ApplicationController
 
     private
 
-    def set_notifications
-        Rails.logger.debug "Params ID: #{params[:id]}"
-        @notification = Notification.joins(:item)
-                                    .find_by(id: params[:id], items: { user_id: current_user.id })
-        Rails.logger.debug "@notification: #{@notification.inspect}"
+    def set_notification
+        @notification = Notification.find(params[:id])
         if @notification.nil?
-            Rails.logger.debug "Notification not found for ID: #{params[:id]}"
             redirect_to items_path, alert: "通知が見つかりません"
         end
     end
 
     def notification_params
-        params.require(:notification).permit(:next_notification_day, :last_notification_day, notification_interval)
+        params.require(:notification).permit(:next_notification_day, :last_notification_day, :notification_interval)
     end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,9 +1,8 @@
 class Notification < ApplicationRecord
   belongs_to :item
 
-  validates :next_notification_day, presence: true
-  validate :past_date_next_notification_day
-  validate :too_far_next_notification_day
+  validate :valid_update_next_notification_day
+
   # 通知日の更新
   def item_update_next_notification_day
     self.update(next_notification_day: item.calculate_next_notification_day)
@@ -16,14 +15,12 @@ class Notification < ApplicationRecord
 
   private
 
-  def past_date_next_notification_day
-    if next_notification_day < Date.today
+  def valid_update_next_notification_day
+    if next_notification_day.blank?
+      errors.add(:next_notification_day, :blank_field)
+    elsif next_notification_day < Date.today + 1.days
       errors.add(:next_notification_day, :past_date)
-    end
-  end
-
-  def too_far_next_notification_day
-    if next_notification_day > Date.today + 365.days
+    elsif next_notification_day > Date.today + 365.days
       errors.add(:next_notification_day, :too_far)
     end
   end
