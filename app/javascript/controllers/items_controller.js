d8af50b39e1e473f453043015eabeac542301791
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-01-23 14:32:14 +0900

    選択したカテゴリーに対応するアイコン画像が表示されるように変更

--- app/javascript/controllers/items_controller.js
+++ app/javascript/controllers/items_controller.js
@@ -1,7 +1,40 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["categorySelect", "unit"];
+  static targets = ["categorySelect", "unit", "select"];
+
+  iconMapping = {
+    'shampoo': '/assets/icons/shampoo.jpg',
+    'body_soap': '/assets/icons/body_soap.jpg',
+    'laundry_detergent_powder': '/assets/icons/laundry_powder.jpg',
+    'laundry_detergent_liquid': '/assets/icons/laundry_liquid.jpg',
+    'fabric_softeners': '/assets/icons/softener.jpg',
+    'dishwashing_detergent': '/assets/icons/dish.jpg',
+    'lotion': '/assets/icons/lotion.jpg',
+    'serum': '/assets/icons/serum.jpg',
+    'moisturising_cream': '/assets/icons/cream.jpg',
+    'face_wash': '/assets/icons/face_wash.jpg',
+    'sunscreen': '/assets/icons/sunscreen.jpg',
+    'others': '/assets/icons/others.jpg'
+  }
+
+  confirmIcon(event) {
+    const selectedCategory = this.categorySelectTarget.value
+    if (!selectedCategory) {
+      event.preventDefault()
+      alert("カテゴリーを選択してください")
+      return false
+    }
+
+    const hidden = document.createElement("input")
+    const iconPath = this.iconMapping[selectedCategory] || this.iconMapping["others"]
+    hiddenIconField.type = "hidden"
+    hiddenIconField.name = "item[icon_path]"
+    hiddenIconField.value = iconPath
+    event.target.appendChild(hiddenIconField)
+
+    return true
+  }
 
   connect() {
     this.updateUnit();
