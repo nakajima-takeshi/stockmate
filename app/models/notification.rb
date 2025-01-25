e50b7a5dd00abc962e2820bb7a83e642bab55025
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-01-25 13:48:01 +0900

    responseを修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -32,6 +32,7 @@ class Notification < ApplicationRecord
           type: "text",
           text: message
         }
+        response = self.push_line_message(line_user_id, message)
         Rails.logger.info("LINE通知送信成功: #{response.body}")
         self.save_last_notification_day
       rescue => error
