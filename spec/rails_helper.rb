7c2d00895913adbcf9908ea74d47716ef91fb3af
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-02-06 23:12:55 +0900

    Userのmodelテストとsystemテストを作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,9 @@ require 'rspec/rails'
 # supportsディレクトリ配下の全てのファイルを読み取り
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
+# OmniAuthのテストモードを有効化
+OmniAuth.config.test_mode = true
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -40,7 +43,7 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
-  config.include LineloginMacros
+  config.include OmniauthMacros
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -85,5 +88,3 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
 end
-# OmniAuthのテストモードを有効化
-OmniAuth.config.test_mode = true
