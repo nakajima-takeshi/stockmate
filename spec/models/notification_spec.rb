c46d7c908b5304942fcebdca336f990f2b02b618
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-02-08 13:02:17 +0900

    notificationモデルのバリデーションテストを作成

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    let(:notification) { build(:notification) }
+
+    it '設定したすべてのバリデーションが機能しているか' do
+      expect(notification).to be_valid
+      expect(notification.errors).to be_empty
+    end
+
+    it '次回通知予定日が今日' do
+      notification = build(:notification, next_notification_day: Date.today)
+      expect(notification).to be_invalid
+      expect(notification.errors.full_messages).to include('通知日は今日以降の日付を入力してください')
+    end
+
+    it '次回通知予定日が空欄' do
+      notification = build(:notification, next_notification_day: nil)
+      expect(notification).to be_invalid
+      expect(notification.errors.full_messages).to include('通知日を入力してください')
+    end
+
+    it '次回通知予定日が365日以上先' do
+      notification = build(:notification, next_notification_day: Date.today + 366.days)
+      expect(notification).to be_invalid
+      expect(notification.errors.full_messages).to include('通知日は１年以内の日付を入力してください')
+    end
+  end
 end
