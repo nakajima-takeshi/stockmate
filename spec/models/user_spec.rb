a41903e20a117bcc1c0d4e709686275eefedb758
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-02-08 10:44:15 +0900

    user_specを編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
     describe 'LINE認証データからユーザーを作成' do
-        before { mock_auth_hash }
+        before do
+            # OmniAuthのモックデータを使って、ユーザーを作成
+            mock_auth_hash
+            @user = User.from_omniauth(auth)
+        end
         let(:auth) { OmniAuth.config.mock_auth[:line] }
 
         context 'ユーザー情報が登録されていない' do
             it 'ユーザー作成' do
-                # OmniAuthのモックデータを使って、ユーザーを作成
-                user = User.from_omniauth(auth)
-                # ユーザーが正しく作成されているか確認
-                expect(user).to be_persisted
+                user = @user
+                expect(user).to be_persisted # DBに存在しているか
                 expect(user.email).to eq('test@example.com')
                 expect(user.name).to eq('Test User')
                 expect(user.provider).to eq('line')
@@ -20,8 +22,9 @@ RSpec.describe User, type: :model do
 
         context 'すでにユーザー情報が登録されている' do
             it '既存のユーザーを取得する' do
-                user = User.from_omniauth(auth)
-                expect( User.from_omniauth(auth) ).to eq(user)
+                user = @user
+                expect(user).to be_persisted
+                expect(user).to eq(user)
             end
         end
     end
