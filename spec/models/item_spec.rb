a054733eda11ce43dc25b2e12f5cc185e3c02aa4
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-02-08 14:07:13 +0900

    contextで分類

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,72 +15,82 @@ RSpec.describe Item, type: :model do
       expect(item.errors).to be_empty
     end
 
-    it 'カテゴリーが選択されていない' do
-      item = build(:item, user: user, category: nil)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("カテゴリーを入力してください")
+    context 'カテゴリーのバリデーション' do
+      it 'カテゴリーが選択されていない' do
+        item = build(:item, user: user, category: nil)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("カテゴリーを入力してください")
+      end
     end
 
-    it '名前が空欄' do
-      item = build(:item, user: user, name: nil)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("名前を入力してください")
+    context '名前のバリデーション' do
+      it '名前が空欄' do
+        item = build(:item, user: user, name: nil)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("名前を入力してください")
+      end
+
+      it '名前が30文字以上' do
+        item = build(:item, user: user, name: "a" * 31)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("名前は30文字以内で入力してください")
+      end
+
+      it '名前が重複' do
+        item_first = create(:item, user: user)
+        item_second = build(:item, user: user, name: "Test_item")
+        expect(item_second).to be_invalid
+        expect(item_second.errors.full_messages).to include("名前はすでに存在しています。重複しないようにしてください")
+      end
     end
 
-    it '名前が30文字以上' do
-      item = build(:item, user: user, name: "a" * 31)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("名前は30文字以内で入力してください")
+    context '内包量のテスト' do
+      it '内包量の入力欄が空' do
+        item = build(:item, user: user, volume: nil)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("内包量を入力してください")
+      end
+
+      it '内包量の値が0である' do
+        item = build(:item, user: user, volume: 0)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("内包量は0以外の値にしてください")
+      end
     end
 
-    it '名前が重複' do
-      item_first = create(:item, user: user)
-      item_second = build(:item, user: user, name: "Test_item")
-      expect(item_second).to be_invalid
-      expect(item_second.errors.full_messages).to include("名前はすでに存在しています。重複しないようにしてください")
+    context '一週間の使用回数のテスト' do
+      it '一週間の使用回数の入力欄が空' do
+        item = build(:item, user: user, used_count_per_weekly: nil)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("一週間の使用回数を入力してください")
+      end
+
+      it '一週間の使用回数の値が0である' do
+        item = build(:item, user: user, used_count_per_weekly: 0)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("一週間の使用回数は0以外の値にしてください")
+      end
+
+      it '一週間の使用回数の値が全角' do
+        item = build(:item, user: user, used_count_per_weekly: "１")
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("一週間の使用回数は数値で入力してください")
+      end
     end
 
-    it '内包量の入力欄が空' do
-      item = build(:item, user: user, volume: nil)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("内包量を入力してください")
-    end
+    context 'メモのテスト' do
+      it 'メモが65535文字以上入力されている' do
+        memo = Faker::Lorem.characters(number: 65_536)
+        item = build(:item, user: user, memo: memo)
+        expect(item).to be_invalid
+        expect(item.errors.full_messages).to include("メモは65,535文字以内で入力してください")
+      end
 
-    it '内包量の値が0である' do
-      item = build(:item, user: user, volume: 0)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("内包量は0以外の値にしてください")
-    end
-
-    it '一週間の使用回数の入力欄が空' do
-      item = build(:item, user: user, used_count_per_weekly: nil)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("一週間の使用回数を入力してください")
-    end
-
-    it '一週間の使用回数の値が0である' do
-      item = build(:item, user: user, used_count_per_weekly: 0)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("一週間の使用回数は0以外の値にしてください")
-    end
-
-    it '一週間の使用回数の値が全角' do
-      item = build(:item, user: user, used_count_per_weekly: "１")
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("一週間の使用回数は数値で入力してください")
-    end
-
-    it 'メモが65535文字以上入力されている' do
-      memo = Faker::Lorem.characters(number: 65_536)
-      item = build(:item, user: user, memo: memo)
-      expect(item).to be_invalid
-      expect(item.errors.full_messages).to include("メモは65,535文字以内で入力してください")
-    end
-
-    it 'メモは空欄でも有効' do
-      item = build(:item, user: user, memo: nil)
-      expect(item).to be_valid
-      expect(item.errors).to be_empty
+      it 'メモは空欄でも有効' do
+        item = build(:item, user: user, memo: nil)
+        expect(item).to be_valid
+        expect(item.errors).to be_empty
+      end
     end
   end
 end
