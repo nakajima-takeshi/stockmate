80c694dd9c4fb40ef4e25095874030cd7e9a33d4
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-02-08 10:44:39 +0900

    itemのファクトリーボット編集

--- spec/factories/item.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-    factory :item do
-        category { "shampoo" }
-        name { "Test_item" }
-        volume { 300 }
-        used_count_per_weekly { 7 }
-        memo { "test" }
-    end
-end
