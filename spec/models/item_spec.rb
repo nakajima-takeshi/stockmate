05b11338367799393b9d928dd5a689725687c48f
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-02-08 13:36:20 +0900

    エラーメッセージをfull_messagesに修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,49 +18,49 @@ RSpec.describe Item, type: :model do
     it 'カテゴリーが選択されていない' do
       item = build(:item, user: user, category: nil)
       expect(item).to be_invalid
-      expect(item.errors[:category]).to include("を入力してください")
+      expect(item.errors.full_messages).to include("カテゴリーを入力してください")
     end
 
     it '名前が空欄' do
       item = build(:item, user: user, name: nil)
       expect(item).to be_invalid
-      expect(item.errors[:name]).to include("を入力してください")
+      expect(item.errors.full_messages).to include("名前を入力してください")
     end
 
     it '名前が30文字以上' do
-      item = build(:item, user: user, name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      item = build(:item, user: user, name: "a" * 31)
       expect(item).to be_invalid
-      expect(item.errors[:name]).to include("は30文字以内で入力してください")
+      expect(item.errors.full_messages).to include("名前は30文字以内で入力してください")
     end
 
-    it '内包量が空欄' do
+    it '内包量の入力欄が空' do
       item = build(:item, user: user, volume: nil)
       expect(item).to be_invalid
-      expect(item.errors[:volume]).to include("を入力してください")
+      expect(item.errors.full_messages).to include("内包量を入力してください")
     end
 
-    it '内包量が0である' do
+    it '内包量の値が0である' do
       item = build(:item, user: user, volume: 0)
       expect(item).to be_invalid
-      expect(item.errors[:volume]).to include("は0以外の値にしてください")
+      expect(item.errors.full_messages).to include("内包量は0以外の値にしてください")
     end
 
-    it '一週間の使用回数が空欄' do
+    it '一週間の使用回数の入力欄が空' do
       item = build(:item, user: user, used_count_per_weekly: nil)
       expect(item).to be_invalid
-      expect(item.errors[:used_count_per_weekly]).to include("を入力してください")
+      expect(item.errors.full_messages).to include("一週間の使用回数を入力してください")
     end
 
-    it '一週間の使用回数が0である' do
+    it '一週間の使用回数の値が0である' do
       item = build(:item, user: user, used_count_per_weekly: 0)
       expect(item).to be_invalid
-      expect(item.errors[:used_count_per_weekly]).to include("は0以外の値にしてください")
+      expect(item.errors.full_messages).to include("一週間の使用回数は0以外の値にしてください")
     end
 
-    it '一週間の使用回数が全角文字' do
+    it '一週間の使用回数の値が全角' do
       item = build(:item, user: user, used_count_per_weekly: "１")
       expect(item).to be_invalid
-      expect(item.errors[:used_count_per_weekly]).to include("は数値で入力してください")
+      expect(item.errors.full_messages).to include("一週間の使用回数は数値で入力してください")
     end
   end
 end
