2a1efd2b7afd766b7f47ecd1ce7434444cd510cc
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-02-09 16:02:50 +0900

    日用品の編集、成功テストを実装

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe Notification, type: :model do
       expect(notification.errors).to be_empty
     end
 
-    it '次回通知予定日が今日' do
+    it '次回通知予定日が今日なので失敗する' do
       notification = build(:notification, next_notification_day: Date.today)
       expect(notification).to be_invalid
       expect(notification.errors.full_messages).to include('通知日は今日以降の日付を入力してください')
     end
 
-    it '次回通知予定日が空欄' do
+    it '次回通知予定日が空欄だと失敗する' do
       notification = build(:notification, next_notification_day: nil)
       expect(notification).to be_invalid
       expect(notification.errors.full_messages).to include('通知日を入力してください')
     end
 
-    it '次回通知予定日が365日以上先' do
+    it '次回通知予定日が365日以上先だと失敗する' do
       notification = build(:notification, next_notification_day: Date.today + 366.days)
       expect(notification).to be_invalid
       expect(notification.errors.full_messages).to include('通知日は１年以内の日付を入力してください')
