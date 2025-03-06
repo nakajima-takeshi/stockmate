9049afca5c6f2448fc711e2d63c8aabc122e552b
nakajima takeshi <nakajimatakeshi.1112@gmail.com>
2025-03-06 18:36:46 +0900

    LINEへの通知メッセージをサービスクラスに移動

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,6 +4,7 @@ class Notification < ApplicationRecord
 
   validate :valid_next_notification_day
 
+  # 通知日を計算する処理群
   def item_update_next_notification_day
     new_notification_day = item.calculate_next_notification_day
     interval = (new_notification_day - Date.today).to_i
@@ -17,10 +18,6 @@ class Notification < ApplicationRecord
                 notification_interval: interval)
   end
 
-  def save_last_notification_day
-    self.update(last_notification_day: Date.today)
-  end
-
   def linebot_update_next_notification_day
     interval_days = item.linebot_calculate_next_notification_day
     new_notification_day = Date.today + interval_days
@@ -29,34 +26,15 @@ class Notification < ApplicationRecord
       notification_interval: interval_days)
   end
 
-  def push_line_message
-    user = item&.user
-    line_user_id = user&.uid
-
-    if line_user_id.present?
-      message = self.create_notification_message
-      begin
-        client.push_message(line_user_id, {
-          type: "text",
-          text: message
-        })
-        self.save_last_notification_day
-      rescue => error
-        Rails.logger.error("LINE通知送信エラー: #{error.message}")
-      end
-    else
-      Rails.logger.error("LINE通知送信エラー: UIDが見つかりません(Notification ID: #{self.id})")
-    end
+  def save_last_notification_day
+    self.update(last_notification_day: Date.today)
   end
 
-  def create_notification_message
-    message = "商品名【#{item.name}】の在庫補充をしてください。\n"
-    message += "カテゴリー : #{I18n.t("categories.#{item.category}", default: item.category)}\n"
-    if item.memo.present?
-      message += "メモ書きがあります。\n"
-      message += "メモ内容 : #{item.memo}\n"
-    end
-    message
+
+  # LINE通知を送信する処理群
+  def push_line_message
+    service = LineNotificationService.new(self)
+    service.call
   end
 
   def self.send_notifications
@@ -68,17 +46,17 @@ class Notification < ApplicationRecord
 
   private
 
+  def self.date_of_notification
+    where(next_notification_day: Date.today)
+  end
+
   def valid_next_notification_day
     if next_notification_day.blank?
       errors.add(:next_notification_day, :blank_field)
-    elsif next_notification_day < Date.today + 1.days
+    elsif next_notification_day < Date.today# + 1.days
       errors.add(:next_notification_day, :past_date)
     elsif next_notification_day > Date.today + 365.days
       errors.add(:next_notification_day, :too_far)
     end
-  end
-
-  def self.date_of_notification
-    where(next_notification_day: Date.today)
   end
 end
